39aa1b99de18be0f714feab2d37ca3a9dd7d0bf3
Gibson Han <gibsonliketheguitar2@gmail.com>
2022-12-28 12:24:25 -0800

    implement modal

--- components/UploadButton/.view.tsx
+++ components/UploadButton/.view.tsx
@@ -1,11 +1,43 @@
-import React, { useState } from "react";
-import { DialogActions, DialogContent, DialogContentText, TextField } from "@mui/material";
+import Image from 'next/image';
+import React, { useRef, useState } from "react";
+import { DialogActions, DialogContent, DialogContentText, TextField, Typography } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
+import { Controller, useForm } from "react-hook-form";
+import Box from "@mui/system/Box";
+
+type T_UploadForm = {
+    title: string;
+    file: File;
+}
 
 export function UploadButton() {
+    const formRef: any = useRef(null)
+
     const [open, setOpen] = useState<boolean>(false)
+    const [preview, setPreview] = useState<string>('')
+
+    const { control, handleSubmit, formState, register, reset } = useForm<T_UploadForm>();
+
+    const handleFileUpload = (e: any) => {
+        console.log('e', e)
+        const file = e.target.files[0]
+
+        setPreview(URL.createObjectURL(file))
+    }
+
+    const handleSubmitClick = () => {
+        if (!formRef) return
+        formRef?.current.dispatchEvent(
+            new Event("submit", { cancelable: true, bubbles: true })
+        );
+    }
+
+    const onSubmit = (data: T_UploadForm) => {
+        console.log('what is data', data)
+    }
 
     const handleOpen = () => {
         setOpen(true)
@@ -20,25 +52,49 @@ export function UploadButton() {
                 Open form dialog
             </Button>
             <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Subscribe</DialogTitle>
+                <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography> Upload</Typography>
+                    <CloseIcon />
+                </DialogTitle>
                 <DialogContent>
                     <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We
-                        will send updates occasionally.
+                        Upload a picture along with a title!
                     </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
+                    <form ref={formRef} onSubmit={handleSubmit(onSubmit)}>
+                        <Controller
+                            name="title"
+                            control={control}
+                            defaultValue=''
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    autoFocus
+                                    margin="dense"
+                                    id="title"
+                                    label="Title"
+                                    type="text"
+                                    fullWidth
+                                    variant="standard"
+                                />
+                            )}
+                        />
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            {preview && <Image alt='uploaded image' src={preview} height={200} width={200} />}
+                            <input
+                                id='imgFile'
+                                accept="image/*"
+                                multiple={false}
+                                type='file'
+                                {...register("file", { required: true })}
+                                onChange={handleFileUpload}
+                            />
+                        </Box>
+                    </form>
+
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Subscribe</Button>
+                    <Button variant='outlined' onClick={handleClose}>Cancel</Button>
+                    <Button type='submit' variant='contained' onClick={handleSubmitClick}>Upload</Button>
                 </DialogActions>
             </Dialog>
         </>
