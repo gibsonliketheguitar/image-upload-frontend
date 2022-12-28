b39494718aeca7df8ea38bed8b829c4d5d76af80
Gibson Han <gibsonliketheguitar2@gmail.com>
2022-12-28 10:44:05 -0800

    add upload button with template dialog component

---
+++ components/UploadButton/.view.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { DialogActions, DialogContent, DialogContentText, TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export function UploadButton() {
+    const [open, setOpen] = useState<boolean>(false)
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+    return (
+        <>
+            <Button variant="outlined" onClick={handleOpen}>
+                Open form dialog
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Subscribe</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To subscribe to this website, please enter your email address here. We
+                        will send updates occasionally.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Email Address"
+                        type="email"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Subscribe</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+}
