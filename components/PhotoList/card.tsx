202f5a1a380656995bebad5a29e7bea3a2a2b165
Gibson Han <gibsonliketheguitar2@gmail.com>
2022-12-28 22:54:11 -0800

    add image card skeleton

--- components/PhotoList/card.tsx
+++ components/PhotoList/card.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
+import { useState } from 'react';
 import styled from '@emotion/styled';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Skeleton } from '@mui/material';
 
 export type T_PhotoCard = {
     title: string,
@@ -14,14 +16,36 @@ const Item = styled(Box)(({ theme }: any) => ({
 
 export function Card(props: T_PhotoCard) {
     const { imgURL, title } = props
+    const [isLoading, setLoading] = useState(true)
+
+    const handleLoading = () => {
+        console.log('Done')
+        setLoading(false)
+    }
     return (
         <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Item>
-                <Image
-                    height={200}
-                    width={300}
-                    src={imgURL}
+            <Item sx={{ position: 'relative', height: '200px', width: '300px' }}>
+                <Image fill
                     alt={title}
+                    onLoadingComplete={handleLoading}
+                    src={imgURL}
+                    style={{
+                        position: 'absolute',
+                        inset: 0,
+                        objectFit: 'cover',
+                    }}
+                />
+                <Skeleton
+                    variant='rectangular'
+                    animation='pulse'
+                    width={300}
+                    height={200}
+                    sx={{
+                        visibility: isLoading ? 'initial' : 'hidden',
+                        position: 'absolute',
+                        inset: 0,
+                        bgcolor: 'grey'
+                    }}
                 />
             </Item>
 
